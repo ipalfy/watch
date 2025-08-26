ea0d0e8861cd42fd9cb821435977b79115b0d90b
Istvan Palfy <ipalfy@googlemail.com>
2025-08-26 14:37:11 +0200

    Partly added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#1e40af',
+      },
+    },
+  },
+  plugins: [],
+};
